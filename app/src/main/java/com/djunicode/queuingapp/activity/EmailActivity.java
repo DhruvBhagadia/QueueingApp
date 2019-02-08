7f639423a0cfce3fd2a738861c40ea8807fe3299
Ruturaj <ruturaj2305@gmail.com>
2019-02-08 09:54:17 +0530

    Generate random string for initial user login

--- app/src/main/java/com/djunicode/queuingapp/activity/EmailActivity.java
+++ app/src/main/java/com/djunicode/queuingapp/activity/EmailActivity.java
@@ -13,57 +13,74 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.djunicode.queuingapp.R;
 import com.djunicode.queuingapp.model.UserEmailVerify;
 import com.djunicode.queuingapp.rest.ApiClient;
 import com.djunicode.queuingapp.rest.ApiInterface;
+
+import java.util.Random;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class EmailActivity extends AppCompatActivity {
 
-  private TextInputLayout signUpEmailTIL, verifyTIL;
-  private EditText signUpEmailEditText, verifyEditText;
-  private Button sendCodeButton, verifyEmailButton;
-  private ApiInterface apiInterface;
-  public static Integer id;
-  private SharedPreferences preferences;
-  private ProgressDialog progressDialog;
+    private TextInputLayout signUpEmailTIL, verifyTIL;
+    private EditText signUpEmailEditText, verifyEditText;
+    private Button sendCodeButton, verifyEmailButton;
+    private ApiInterface apiInterface;
+    public static Integer id;
+    private SharedPreferences preferences;
+    private ProgressDialog progressDialog;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_email);
+    public String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
 
-    apiInterface = ApiClient.getClient().create(ApiInterface.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
 
-    Intent intent = getIntent();
-    final String user = intent.getStringExtra("user");
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
 
-    signUpEmailTIL = (TextInputLayout) findViewById(R.id.signUpEmailTIL);
-    verifyTIL = (TextInputLayout) findViewById(R.id.verifyTIL);
-    signUpEmailEditText = (EditText) findViewById(R.id.signUpEmailEditText);
-    verifyEditText = (EditText) findViewById(R.id.verifyEditText);
-    sendCodeButton = (Button) findViewById(R.id.sendCodeButton);
-    verifyEmailButton = (Button) findViewById(R.id.verifyEmailButton);
-    preferences = this.getSharedPreferences("com.djunicode.queuingapp", MODE_PRIVATE);
+        Intent intent = getIntent();
+        final String user = intent.getStringExtra("user");
 
-    verifyEditText.setEnabled(false);
-    verifyEditText.setAlpha(0.4f);
+        signUpEmailTIL = (TextInputLayout) findViewById(R.id.signUpEmailTIL);
+        verifyTIL = (TextInputLayout) findViewById(R.id.verifyTIL);
+        signUpEmailEditText = (EditText) findViewById(R.id.signUpEmailEditText);
+        verifyEditText = (EditText) findViewById(R.id.verifyEditText);
+        sendCodeButton = (Button) findViewById(R.id.sendCodeButton);
+        verifyEmailButton = (Button) findViewById(R.id.verifyEmailButton);
+        preferences = this.getSharedPreferences("com.djunicode.queuingapp", MODE_PRIVATE);
 
-    verifyEmailButton.setEnabled(false);
+        verifyEditText.setEnabled(false);
+        verifyEditText.setAlpha(0.4f);
 
-    sendCodeButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        progressDialog = ProgressDialog
-            .show(EmailActivity.this, "Sending code.", "Please wait...");
-        String email = signUpEmailEditText.getText().toString();
+        verifyEmailButton.setEnabled(false);
+
+        sendCodeButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = ProgressDialog
+                        .show(EmailActivity.this, "Sending code.", "Please wait...");
+                String email = signUpEmailEditText.getText().toString();
 //        email.split("@")[0]
-        Call<UserEmailVerify> call = apiInterface.sendEmail("swappers4", email, "demopass");
-        if (user.equals("teacher")) {
-          if (email.contains("@djsce.ac.in")) {
+
+                Call<UserEmailVerify> call = apiInterface.sendEmail(getSaltString(), email, "demopass");
+                if (user.equals("teacher")) {
+                    if (email.contains("@djsce.ac.in")) {
             /*call.enqueue(new Callback<UserEmailVerify>() {
               @Override
               public void onResponse(@NonNull Call<UserEmailVerify> call,
@@ -82,100 +99,100 @@ public class EmailActivity extends AppCompatActivity {
                 Log.e("Error:", t.getMessage());
               }
             });*/
-          } else {
-            Log.e("Error: ", "Invalid teacher email");
-          }
-          call.enqueue(new Callback<UserEmailVerify>() {
-            @Override
-            public void onResponse(@NonNull Call<UserEmailVerify> call,
-                @NonNull Response<UserEmailVerify> response) {
-              try {
-                id = response.body().getId();
-                preferences.edit().putInt("teacherUserID", id).apply();
-                Log.i("ID:", Integer.toString(id));
-                verifyEmailButton.setEnabled(true);
-                progressDialog.dismiss();
-                Toast.makeText(EmailActivity.this, "Code sent, please check your email.",
-                    Toast.LENGTH_LONG).show();
-                verifyEditText.setEnabled(true);
-                verifyEditText.setAlpha(1.0f);
-              } catch (Exception e) {
-                Toast.makeText(EmailActivity.this, "Invalid e-mail", Toast.LENGTH_LONG).show();
-                Log.e("TeEx", e.getMessage());
-                progressDialog.dismiss();
-              }
-            }
+                    } else {
+                        Log.e("Error: ", "Invalid teacher email");
+                    }
+                    call.enqueue(new Callback<UserEmailVerify>() {
+                        @Override
+                        public void onResponse(@NonNull Call<UserEmailVerify> call,
+                                               @NonNull Response<UserEmailVerify> response) {
+                            try {
+                                id = response.body().getId();
+                                preferences.edit().putInt("teacherUserID", id).apply();
+                                Log.i("ID:", Integer.toString(id));
+                                verifyEmailButton.setEnabled(true);
+                                progressDialog.dismiss();
+                                Toast.makeText(EmailActivity.this, "Code sent, please check your email.",
+                                        Toast.LENGTH_LONG).show();
+                                verifyEditText.setEnabled(true);
+                                verifyEditText.setAlpha(1.0f);
+                            } catch (Exception e) {
+                                Toast.makeText(EmailActivity.this, "Invalid e-mail", Toast.LENGTH_LONG).show();
+                                Log.e("TeEx", e.getMessage());
+                                progressDialog.dismiss();
+                            }
+                        }
 
-            @Override
-            public void onFailure(Call<UserEmailVerify> call, Throwable t) {
-              Log.e("Error:", t.getMessage());
-            }
-          });
-        } else {
-          call.enqueue(new Callback<UserEmailVerify>() {
-            @Override
-            public void onResponse(Call<UserEmailVerify> call, Response<UserEmailVerify> response) {
-              try {
-                id = response.body().getId();
-                preferences.edit().putInt("studentID", id).apply();
-                Log.e("ID:", id.toString());
-                verifyEmailButton.setEnabled(true);
-                progressDialog.dismiss();
-                Toast.makeText(EmailActivity.this, "Code sent, please check your email.",
-                    Toast.LENGTH_LONG).show();
-                verifyEditText.setEnabled(true);
-                verifyEditText.setAlpha(1.0f);
-              } catch (Exception e) {
-                Toast.makeText(EmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-              }
-            }
-
-            @Override
-            public void onFailure(Call<UserEmailVerify> call, Throwable t) {
-              Log.e("Error:", t.getMessage());
-            }
-          });
-        }
-        sendCodeButton.setEnabled(false);
-      }
-    });
-
-    verifyEmailButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        progressDialog = ProgressDialog
-            .show(EmailActivity.this, "Verifying.", "Please wait...");
-        final Intent intent = new Intent(EmailActivity.this, LogInActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        Call<UserEmailVerify> call = apiInterface
-            .verifyEmail(id, verifyEditText.getText().toString());
-        call.enqueue(new Callback<UserEmailVerify>() {
-          @Override
-          public void onResponse(Call<UserEmailVerify> call, Response<UserEmailVerify> response) {
-            try {
-              Boolean valid = response.body().getValid();
-              Log.e("Valid:", valid.toString());
-              progressDialog.dismiss();
-              if (valid) {
-                if (user.equals("teacher")) {
-                  intent.putExtra("user", "teacher");
+                        @Override
+                        public void onFailure(Call<UserEmailVerify> call, Throwable t) {
+                            Log.e("Error:", t.getMessage());
+                        }
+                    });
                 } else {
-                  intent.putExtra("user", "student");
-                }
-                startActivity(intent);
-              } else {
-                Toast.makeText(EmailActivity.this, "Invalid Token!", Toast.LENGTH_SHORT).show();
-              }
-            } catch (Exception e) {
-              Toast.makeText(EmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-          }
+                    call.enqueue(new Callback<UserEmailVerify>() {
+                        @Override
+                        public void onResponse(Call<UserEmailVerify> call, Response<UserEmailVerify> response) {
+                            try {
+                                id = response.body().getId();
+                                preferences.edit().putInt("studentID", id).apply();
+                                Log.e("ID:", id.toString());
+                                verifyEmailButton.setEnabled(true);
+                                progressDialog.dismiss();
+                                Toast.makeText(EmailActivity.this, "Code sent, please check your email.",
+                                        Toast.LENGTH_LONG).show();
+                                verifyEditText.setEnabled(true);
+                                verifyEditText.setAlpha(1.0f);
+                            } catch (Exception e) {
+                                Toast.makeText(EmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
 
-          @Override
-          public void onFailure(Call<UserEmailVerify> call, Throwable t) {
-            Log.e("Error:", t.getMessage());
-          }
+                        @Override
+                        public void onFailure(Call<UserEmailVerify> call, Throwable t) {
+                            Log.e("Error:", t.getMessage());
+                        }
+                    });
+                }
+                sendCodeButton.setEnabled(false);
+            }
         });
+
+        verifyEmailButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = ProgressDialog
+                        .show(EmailActivity.this, "Verifying.", "Please wait...");
+                final Intent intent = new Intent(EmailActivity.this, LogInActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                Call<UserEmailVerify> call = apiInterface
+                        .verifyEmail(id, verifyEditText.getText().toString());
+                call.enqueue(new Callback<UserEmailVerify>() {
+                    @Override
+                    public void onResponse(Call<UserEmailVerify> call, Response<UserEmailVerify> response) {
+                        try {
+                            Boolean valid = response.body().getValid();
+                            Log.e("Valid:", valid.toString());
+                            progressDialog.dismiss();
+                            if (valid) {
+                                if (user.equals("teacher")) {
+                                    intent.putExtra("user", "teacher");
+                                } else {
+                                    intent.putExtra("user", "student");
+                                }
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(EmailActivity.this, "Invalid Token!", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (Exception e) {
+                            Toast.makeText(EmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserEmailVerify> call, Throwable t) {
+                        Log.e("Error:", t.getMessage());
+                    }
+                });
 
         /*if (user.equals("teacher")) {
           intent.putExtra("user", user);
@@ -184,7 +201,7 @@ public class EmailActivity extends AppCompatActivity {
         }
         startActivity(intent);*/
 
-      }
-    });
-  }
+            }
+        });
+    }
 }
